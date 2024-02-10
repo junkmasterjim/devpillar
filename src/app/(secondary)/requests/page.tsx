ad2e09d9ec7df6e33642a796e3b7eaba1b3f6e0f
Noah Pittman <noahpittman00@gmail.com>
2024-02-10 13:52:58 -0500

    Refactor loading and error handling in Requests component

--- src/app/(secondary)/requests/page.tsx
+++ src/app/(secondary)/requests/page.tsx
@@ -2,6 +2,7 @@
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 import {
 	Card,
 	CardContent,
@@ -23,12 +24,10 @@ export interface ResourceRequest {
 }
 
 export default function Requests() {
-	const [loading, setLoading] = useState(false);
-	const [requests, setRequests] = useState<ResourceRequest[]>([]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [requests, setRequests] = useState<ResourceRequest[]>();
 
 	const getData = async () => {
-		setLoading(true);
-
 		const supabase = createClient();
 		const { data: resourceRequests } = await supabase
 			.from("resourceRequests")
@@ -36,50 +35,84 @@ export default function Requests() {
 
 		resourceRequests && setRequests(resourceRequests);
 
-		setLoading(false);
+		setTimeout(() => setLoading(false), 500);
 	};
 
-	useEffect(() => {
-		getData();
-	}, []);
+	getData();
 
-	return (
-		<div className="flex flex-wrap gap-2 justify-center">
-			{requests?.length === 0 && (
+	if (loading) {
+		return (
+			<div className="flex justify-center items-center h-screen">
+				<div className="animate-spin rounded-full h-32 w-32 border-b-2 border-t-2 border-primary" />
+			</div>
+		);
+	}
+
+	if (!requests) {
+		return (
+			<div className="flex justify-center items-center h-screen">
+				<p className="text-center text-muted-foreground">
+					Error fetching resource requests.
+				</p>
+			</div>
+		);
+	}
+
+	if (requests && requests.length === 0) {
+		return (
+			<div className="flex justify-center items-center h-screen">
 				<p className="text-center text-muted-foreground">
 					No resource requests found.
 				</p>
-			)}
+			</div>
+		);
+	}
 
-			{requests?.map((request: ResourceRequest) => (
-				<Card
-					key={request.id}
-					className="max-w-sm w-full flex justify-between flex-col"
+	if (requests && requests.length > 0) {
+		return (
+			<div className="flex flex-wrap gap-2 justify-center">
+				{requests && requests?.length === 0 && (
+					<p className="text-center text-muted-foreground">
+						No resource requests found.
+					</p>
+				)}
+				<motion.div
+					initial={{ opacity: 0, y: 10 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ duration: 0.3 }}
+					className="grid grid-cols-3 gap-4"
 				>
-					<CardHeader>
-						<CardTitle>
-							<span className="text-muted-foreground">{request.id}. </span>
-							{request.name}
-						</CardTitle>
-						<CardDescription>{request.description}</CardDescription>
-					</CardHeader>
-					<CardContent className="space-y-2">
-						<Button asChild size={"sm"} variant={"outline"}>
-							<Link href={request.url} target="_blank">
-								<ArrowUpRightFromSquare className="h-5 w-5 mr-2" /> Visit
-								Resource
-							</Link>
-						</Button>
-						<p>
-							{request.categories.map((cat) => (
-								<Badge key={Math.random()} variant={"secondary"}>
-									{cat}
-								</Badge>
-							))}
-						</p>
-					</CardContent>
-				</Card>
-			))}
-		</div>
-	);
+					{requests?.map((request: ResourceRequest) => (
+						<Card
+							key={request.id}
+							className="max-w-sm w-full flex justify-between flex-col"
+						>
+							<CardHeader>
+								<CardTitle>
+									<span className="text-muted-foreground">{request.id}. </span>
+									{request.name}
+								</CardTitle>
+								<CardDescription>{request.description}</CardDescription>
+							</CardHeader>
+							<CardContent className="space-y-2">
+								<Button asChild size={"sm"} variant={"outline"}>
+									<Link href={request.url} target="_blank">
+										<ArrowUpRightFromSquare className="h-5 w-5 mr-2" /> Visit
+										Resource
+									</Link>
+								</Button>
+								<p>
+									{request.categories.map((cat) => (
+										<Badge key={Math.random()} variant={"secondary"}>
+											{cat}
+										</Badge>
+									))}
+								</p>
+							</CardContent>
+						</Card>
+					))}
+				</motion.div>
+			</div>
+		);
+	}
 }
