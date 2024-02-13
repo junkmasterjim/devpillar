a6fc3418105af00369c3c7ffebeeca27916aae15
Noah Pittman <noahpittman00@gmail.com>
2024-02-12 20:08:35 -0500

    Update search page

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -9,6 +9,7 @@ import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Reply } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 const Page = () => {
 	const searchParams = useSearchParams();
@@ -46,56 +47,62 @@ const Page = () => {
 			{params && (
 				<>
 					<ScrollToTop />
-					{searchResults.length === 0 ? (
-						<div className="flex flex-col gap-2 justify-center h-full items-center text-muted-foreground">
-							<p className="font-medium text-lg ">Sorry! No results found.</p>
-							<p className="text-sm">
-								Try different keywords, or{" "}
-								<span
-									onClick={() =>
-										document.getElementById("add-resource")?.click()
-									}
-									className="border-b border-muted-foreground cursor-pointer hover:text-foreground hover:border-foreground transition-colors"
-								>
-									request a new resource.
-								</span>
-							</p>
 
-							<Button
-								variant={"ghost"}
-								className="mt-4 text-muted-foreground"
-								asChild
-							>
-								<Link href="/">
-									<Reply className="mr-2" />
-									Return to Home
-								</Link>
-							</Button>
-						</div>
+					{searchResults.length === 0 ? (
+						<div className="flex flex-col gap-2 justify-center h-full items-center text-muted-foreground"></div>
 					) : (
-						<motion.section
+						<></>
+					)}
+					<motion.div
+						initial={{ opacity: 0, y: 10 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.3 }}
+						className="pb-4 w-fit"
+					>
+						<h2 className="md:text-3xl text-xl font-semibold text-muted-foreground py-2">
+							Results for "{params}"{" "}
+							<span className="opacity-50">({searchResults.length})</span>
+						</h2>
+						<Separator />
+					</motion.div>
+					{searchResults.length === 0 && (
+						<motion.p
 							initial={{ opacity: 0, y: 10 }}
 							animate={{ opacity: 1, y: 0 }}
 							transition={{ duration: 0.3 }}
-							className="gap-4 grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 mx-auto"
+							className="text-sm text-muted-foreground"
 						>
-							{searchResults
-								.sort((a: Resource, b: Resource) => {
-									return a.name.localeCompare(b.name);
-								})
-								.map((resource: Resource) => (
-									<ResourceCard
-										key={Math.random()}
-										name={resource.name}
-										category={resource.category}
-										description={resource.description}
-										paid={resource.paid}
-										url={resource.url}
-										image={resource?.image}
-									/>
-								))}
-						</motion.section>
+							Try different keywords, or{" "}
+							<span
+								onClick={() => document.getElementById("add-resource")?.click()}
+								className="border-b border-muted-foreground cursor-pointer hover:text-foreground hover:border-foreground transition-colors"
+							>
+								request a new resource.
+							</span>
+						</motion.p>
 					)}
+					<motion.section
+						initial={{ opacity: 0, y: 10 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.3 }}
+						className="gap-4 grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 mx-auto"
+					>
+						{searchResults
+							.sort((a: Resource, b: Resource) => {
+								return a.name.localeCompare(b.name);
+							})
+							.map((resource: Resource) => (
+								<ResourceCard
+									key={Math.random()}
+									name={resource.name}
+									category={resource.category}
+									description={resource.description}
+									paid={resource.paid}
+									url={resource.url}
+									image={resource?.image}
+								/>
+							))}
+					</motion.section>
 				</>
 			)}
 		</>
